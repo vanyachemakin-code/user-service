e58ac54e09c626987dba2ff2ea9f359ca3ada1c6
vanyachemakin <work.vanyachemkin@gmail.com>
2026-08-15 23:28:33 +0300

    add TestPropertySource in TestFallbackController

--- gateway-service/src/test/java/gateway/controller/TestFallbackController.java
+++ gateway-service/src/test/java/gateway/controller/TestFallbackController.java
@@ -6,10 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.config.import=optional:configserver:",
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false",
+
+        "spring.cloud.gateway.routes[0].id=user-service",
+        "spring.cloud.gateway.routes[0].uri=http://localhost:9999",
+        "spring.cloud.gateway.routes[0].predicates[0]=Path=/api/v1/user-service/**",
+        "spring.cloud.gateway.routes[0].filters[0].name=CircuitBreaker",
+        "spring.cloud.gateway.routes[0].filters[0].args.name=userServiceCircuitBreaker",
+        "spring.cloud.gateway.routes[0].filters[0].args.fallbackUri=forward:/fallback/user-service",
+
+        "resilience4j.circuitbreaker.instances.userServiceCircuitBreaker.sliding-window-size=5",
+        "resilience4j.circuitbreaker.instances.userServiceCircuitBreaker.failure-rate-threshold=50",
+        "resilience4j.circuitbreaker.instances.userServiceCircuitBreaker.wait-duration-in-open-state=1s",
+        "resilience4j.circuitbreaker.instances.userServiceCircuitBreaker.permitted-number-of-calls-in-half-open-state=3"
+})
 public class TestFallbackController {
 
     @Autowired
